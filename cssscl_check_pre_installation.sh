e3643830127772da2a97ba606f46f73201340c92
iborozan <ivan.borozan@gmail.com>
2015-09-10 13:46:40 -0400

    added to  cssscl_check_pre_installation.sh a note

--- cssscl_check_pre_installation.sh
+++ cssscl_check_pre_installation.sh
@@ -166,7 +166,7 @@ else
 	    echo "-----------------------------------------------------"
 	    echo "| The installation log file $pwd/log_install.txt indicates that :"
 	    echo "| ${installed_progs[*]} have been installed by this program in $pwd/src/bin but are not currently in the PATH."
-	    echo "| NOTE                                                                                    :"
+	    echo "| NOTE :"
 	    echo "| YOU NEED TO ADD $pwd/src/bin to YOUR PATH VARIABLE BY SOURCING THE SCRIPT AS SHOWN BELOW:"
             echo "---------------------------------"
             echo "  source $pwd/scripts/export.sh  "
